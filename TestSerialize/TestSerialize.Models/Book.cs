dfe786bad95980e09415367b1b8f7290fde5cbfc
chouchao <lllcto@gmail.com>
2013-06-27 12:14:02 +0800

    TestSerialize 1

---
+++ TestSerialize/TestSerialize.Models/Book.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TestSerialize.Models
+{
+    public class Book
+    {
+        public int Id { get; set; }
+
+        public decimal Price { get; set; }
+
+        public string Name { get; set; }
+
+        public bool IsNew { get; set; }
+
+        public string Url { get; set; }
+    }
+}
