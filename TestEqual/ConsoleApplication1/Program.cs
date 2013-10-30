e158c9d69adeba6693dbbbc05cdd208ef2ba78ed
chouchao <lllcto@gmail.com>
2013-10-30 17:38:55 +0800

    TestEqual & testjs

---
+++ TestEqual/ConsoleApplication1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s1 = "1";
+            int i1 = 1;
+
+            if (i1 > 0 && (s1 = "2") == "1")
+            {
+                Console.Write("y,");
+            }
+            else
+            {
+                Console.Write("n,");
+            }
+
+            Console.WriteLine(String.Format("s1={0}", s1));
+        }
+    }
+}
