3437498ff8e2918dfe5462e5a935d45ded768952
chouchao <lllcto@gmail.com>
2014-08-17 11:18:49 +0800

    format

--- TestEqual/ConsoleApplication1/Program.cs
+++ TestEqual/ConsoleApplication1/Program.cs
@@ -10,7 +10,7 @@ namespace ConsoleApplication1
         static void Main(string[] args)
         {
             string s1 = "1";
-            int i1 = 1;
+            int i1 = 12;
 
             if (i1 > 0 && (s1 = "2") == "1")
             {
@@ -21,7 +21,39 @@ namespace ConsoleApplication1
                 Console.Write("n,");
             }
 
-            Console.WriteLine(String.Format("s1={0}", s1));
+            Console.WriteLine(String.Format("i1={0:X000}", i1));
         }
     }
+
+
+    //public abstract class A
+    //{
+    //    public A()
+    //    {
+    //        Console.Write("A,");
+    //    }
+    //    public virtual void Fun()
+    //    {
+    //        Console.WriteLine("A.Fun()");
+    //    }
+    //}
+
+    //public class B : A
+    //{
+    //    public B()
+    //    {
+    //        Console.Write("B,");
+    //    }
+
+    //    public new void Fun()
+    //    {
+    //        Console.WriteLine("B.Fun()");
+    //    }
+
+    //    public static void Main()
+    //    {
+    //        A a = new B();
+    //        a.Fun();
+    //    }
+    //}   
 }
